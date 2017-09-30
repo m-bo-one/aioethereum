1f2dfc632f7693d0b6bdb5a4f47b494288a0c7e5
DeV1doR <bogdankurinniy.dev1@gmail.com>
2017-09-30 13:47:17 +0300

    Add autoreconnect for socket and base client

--- aioethereum/__init__.py
+++ aioethereum/__init__.py
@@ -1,8 +1,10 @@
-from .client import AsyncIOHTTPClient
+from .client import AsyncIOHTTPClient, AsyncIOIPCClient, create_ethereum_client
 
 
 __version__ = '0.0.1'
 
 __all__ = [
     'AsyncIOHTTPClient',
+    'AsyncIOIPCClient',
+    'create_ethereum_client',
 ]
--- aioethereum/client.py
+++ aioethereum/client.py
@@ -1,5 +1,6 @@
 import logging
 import asyncio
+from urllib.parse import urlparse
 
 import aiohttp
 import async_timeout
@@ -16,13 +17,19 @@ from .management import RpcMixin
 logger = logging.getLogger('asyncio_client')
 
 
+_reconnect_times = 3
+
+
 class AsyncIOIPCClient(RpcMixin):
 
-    def __init__(self, unix_path, timeout=60, *, loop=None):
-        self.unix_path = unix_path
+    def __init__(self, reader, writer, unix_path, timeout=60, *, loop=None):
+        self._reader = reader
+        self._writer = writer
         self._timeout = timeout
+        self._unix_path = unix_path
         self._id = 1
         self._loop = loop or asyncio.get_event_loop()
+        self._lock = asyncio.Semaphore(1, loop=self._loop)
 
     @asyncio.coroutine
     def _call(self, method, params=None, _id=None):
@@ -37,9 +44,23 @@ class AsyncIOIPCClient(RpcMixin):
             self._writer.write(json.dumps(data).encode('utf-8'))
             b = yield from self._reader.readline()
             if not b:
-                self._log.error('_receive: no data, connection refused.')
-                raise ConnectionError('Didn\'t receive any data, '
-                                      'connection refused.')
+                tried = 0
+                while True:
+                    try:
+                        new_client = create_ethereum_client(self._unix_path,
+                                                            self._timeout)
+                    except Exception:
+                        tried += 1
+                        if tried >= _reconnect_times:
+                            self._log.error('_receive: no data, '
+                                            'connection refused.')
+                            raise ConnectionError('Didn\'t receive any data, '
+                                                  'connection refused.')
+                self._id = new_client._id
+                self._reader = new_client._reader
+                self._writer = new_client._writer
+                self._lock = new_client._lock
+                return self._call(method, params, _id)
 
         try:
             response = json.loads(b.decode('utf-8'))
@@ -107,3 +128,23 @@ class AsyncIOHTTPClient(RpcMixin):
         except KeyError:
             raise BadResponseError(response['error']['message'],
                                    response['error']['code'])
+
+
+@asyncio.coroutine
+def create_ethereum_client(uri, timeout=60, *, loop=None):
+    presult = urlparse(uri)
+    if presult.scheme in ('ipc', 'unix'):
+        reader, writer = yield from asyncio.open_unix_connection(presult.path)
+        return AsyncIOIPCClient(reader, writer, uri, timeout, loop=loop)
+    elif presult.scheme in ('http', 'https'):
+        tls = False
+        if presult.scheme[-1] == 's':
+            tls = True
+        netloc = presult.netloc.split(':')
+        if len(netloc) == 1:
+            host, port = netloc[0], 80
+        else:
+            host, port = netloc
+        return AsyncIOHTTPClient(host, port, tls, timeout, loop=loop)
+    else:
+        raise RuntimeError('This scheme does not supported.')
